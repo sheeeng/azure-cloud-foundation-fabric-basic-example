31b9511810aba764176c066649119c3c434ff517
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2022-10-11 12:03:54 +0200

    feature: cloud adoption framework simple example

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+provider "azurerm" {
+  alias = "vhub"
+}
+
 module "caf" {
   source  = "aztfmod/caf/azurerm"
-  version = "5.4.8"
+  version = "~>5.6.2"
 
   global_settings = var.global_settings
   resource_groups = var.resource_groups

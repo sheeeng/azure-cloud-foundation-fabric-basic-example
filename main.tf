e94990f52aa6c0950d41e6363237f43e5e730ddf
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2022-10-11 10:41:11 +0200

    feature: cloud adoption framework simple example

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "caf" {
   source  = "aztfmod/caf/azurerm"
-  version = "~>5.6.2"
+  version = "5.4.8"
 
   global_settings = var.global_settings
   resource_groups = var.resource_groups
--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.25.0"
-      configuration_aliases = [
-        azurerm.vhub
-      ]
-    }
-  }
-  required_version = ">= 1.3.1"
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.25.0"
+#       configuration_aliases = [
+#         azurerm.vhub
+#       ]
+#     }
+#   }
+#   required_version = ">= 1.3.1"
+# }
